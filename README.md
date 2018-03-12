604836816b24cdd3dbecd8f52f6aa9ab8d899160
afingerhut <achim.fingerhut@live.de>
2018-03-12 18:05:29 +0100

    Initialization

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CSharp-client-for-TensorFlow
 Access a TensorFlow service from a c# client using the gRPC protocol
+
+Using a trained TensorFlow network in in a python or java environment or even in a android app is well documented and there are lots of examples which will pave you the way.
+
---
+++ protocol_compiler/protoc.bat
@@ -0,0 +1,27 @@
+
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ feature.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ example.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ input.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ wrappers.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ model.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ classification.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ any.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ resource_handle.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ tensor_shape.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ types.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ tensor.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ attr_value.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ node_def.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ op_def.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ function.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ versions.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ graph.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ saver.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ meta_graph.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ get_model_metadata.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ regression.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ inference.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ predict.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+protoc.exe --csharp_out ../cs_classes --grpc_out ../cs_classes --proto_path ../protocol_buffer_files/ prediction_service.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe 
+
+pause
